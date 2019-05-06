031f944874bff9739ce0382c9071af124fd2f742
scryan <47368744+scryan@users.noreply.github.com>
2019-05-05 21:46:01 -0400

    Added Clicktracker via upload.

---
+++ ClickTracker.cpp
@@ -0,0 +1,18 @@
+/*
+ * ClickTracker.cpp
+ *
+ *  Created on: May 4, 2019
+ *      Author: Samma
+ */
+
+#include "ClickTracker.h"
+
+ClickTracker::ClickTracker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ClickTracker::~ClickTracker() {
+	// TODO Auto-generated destructor stub
+}
+
